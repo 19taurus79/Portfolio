5d76be7c247ca8b6b9568bf72507e0c4ec0547e2
Onyshchenko Serhii <123294835+19taurus79@users.noreply.github.com>
2026-04-18 19:52:23 +0300

    Configure GitHub Pages for Portfolio

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/Portfolio/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
